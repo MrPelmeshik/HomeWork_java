dc25f2175edcacab9868fabffc9ed052b85295ff
Ilya Fedorov <ilya-fedorov-2000@list.ru>
2021-10-10 17:13:45 +0500

    Сделал задание 2 (лр 1)

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,23 @@ public class Main {
         float var_float = 1.1F;
         char var_char = '1';
         String var_string = "11";
+
+        // Задание 1-2
+        System.out.print("Task 1-2:\nInput:\n\ta -> ");
+        double a = sc.nextDouble();
+        System.out.print("\tb -> ");
+        double b = sc.nextDouble();
+        System.out.print("\tc -> ");
+        double c = sc.nextDouble();
+        System.out.print("\td -> ");
+        double d = sc.nextDouble();
+
+        double res_1 = CalcMethodForTask_1_2(a, b, c, d);
+        System.out.print("Result ->\n\t" + res_1);
+    }
+
+    public static double CalcMethodForTask_1_2 (double a, double b, double c, double d) {
+        return a * (b + (c/d));
     }
 
 }
