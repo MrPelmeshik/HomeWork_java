4c90ba8c4c21ebdd3cd3740e61a30a9a48378622
Ilya Fedorov <ilya-fedorov-2000@list.ru>
2021-10-10 17:13:10 +0500

    Сделал задание 1 (лр 1)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Ok...");
+        Scanner sc = new Scanner(System.in);
+
+        //Задание 1-1
+        boolean var_boolean = true;
+        byte vat_byte = 1;
+        short var_short = 1;
+        int var_int = 1;
+        long var_long = 1;
+        double var_double = 1.1;
+        float var_float = 1.1F;
+        char var_char = '1';
+        String var_string = "11";
     }
 
 }
