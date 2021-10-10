3f8fce0f24175dea4bd860bd38f960ced5b75ba2
Ilya Fedorov <ilya-fedorov-2000@list.ru>
2021-10-10 17:28:47 +0500

    Сделал задания 1 (лр 2)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Ok...");
+        Scanner sc = new Scanner(System.in);
+
+        //Задание 2-1
+        int[] arr_1 = {1, 0, 1, 1, 0, 1, 0, 0, 0, 1};
+        System.out.print("\n\nTask 2-1\nStart values:\n\tarr -> [ ");
+        for (int j : arr_1) {
+            System.out.print(j + " ");
+        }
+        System.out.print(" ]\nResult ->\n\tarr -> [ ");
+        for (int i =0; i < arr_1.length; i++) {
+            if (arr_1[i] == 0)
+                arr_1[i] = 1;
+            else
+                arr_1[i] = 0;
+            System.out.print(arr_1[i] + " ");
+        }
+        System.out.print("]");
     }
 
 }
