c5bd7674bf12d4bf4f4b38caa53544987aea575c
Ilya Fedorov <ilya-fedorov-2000@list.ru>
2021-10-10 17:29:30 +0500

    Сделал задания 2 (лр 2)

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,15 @@ public class Main {
             System.out.print(arr_1[i] + " ");
         }
         System.out.print("]");
+
+        //Задание 2-2
+        int[] arr_2 = new int[8];
+        System.out.print("\n\nTask 2-2\nResult ->\n\tarr -> [ ");
+        for (int i = 0; i < 8; i++) {
+            arr_2[i] = 3 * i;
+            System.out.print(arr_2[i] + " ");
+        }
+        System.out.print("]");
     }
 
 }
